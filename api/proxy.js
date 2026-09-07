6810162a96fa46967999c3de97901a2474abc6c6
hyperspacetutors <admin@hyperspacetutors.com>
2026-09-07 14:43:42 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,8 +3,15 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwP7J6U1NtK7araBy2U4RO51bxgjs6w3wc4LjJb-_rF9TCYrcVRaV-GwC-G7QFB-9r1IQ/exec');
-    const data = await response.json();
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwP7J6U1NtK7araBy2U4RO51bxgjs6w3wc4LjJb-_rF9TCYrcVRaV-GwC-G7QFB-9r1IQ/exec', {
+      redirect: 'follow',
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+    const text = await response.text();
+    console.log('Raw response:', text.substring(0, 200));
+    const data = JSON.parse(text);
     res.status(200).json(data);
   } catch(err) {
     res.status(500).json({ error: err.message });
